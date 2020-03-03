7aef537310916207ec01f15e1b7d2df5550c501e
klambert <kevleyski@gmail.com>
2020-03-03 21:29:59 +1100

    Jenkins trigger test

--- main.cpp
+++ main.cpp
@@ -237,6 +237,34 @@ int main_vector() {
   return 0;
 }
 
+// Function to get the missing number
+int getMissingNo(int a[], int n)
+{
+  // For xor of all the elements in array
+  int x1 = a[0];
+
+  // For xor of all the elements from 1 to n+1
+  int x2 = 1;
+
+  for (int i = 1; i < n; i++)
+    x1 = x1 ^ a[i];
+
+  for (int i = 2; i <= n + 1; i++)
+    x2 = x2 ^ i;
+
+  return (x1 ^ x2);
+}
+
+// Driver Code
+int main_missing()
+{
+  int arr[] = { 1, 2, 4, 5, 6 };
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int miss = getMissingNo(arr, n);
+  cout << miss;
+}
+
+
 int main() {
 
   Foo *foo = new Foo();
@@ -516,6 +544,8 @@ int main() {
   }
 
   main_vector();
+
+  main_missing();
 
   return 0;
 }
