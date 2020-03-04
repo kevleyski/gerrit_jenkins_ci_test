9cdceeff95786f257744d6ceda2ef446c451b896
Kevin Staunton-Lambert <kevleyski@gmail.com>
2020-03-05 09:30:42 +1100

    Jenkins trigger test

--- kevs3.cpp
+++ kevs3.cpp
@@ -3,13 +3,19 @@
 
 using namespace std;
 
-int main() {
+int main_3() {
   map<int, string> kevs;
   kevs.insert(pair<int, string>(5, "erk"));
   kevs.insert(pair<int, string>(20, "flip"));
+  kevs.insert(pair<int, string>(2, "two"));
+  kevs.insert(pair<int, string>(1, "one"));
 
   auto it = kevs.begin();
   for (; it != kevs.end(); it++) {
-    cout << it->first << endl;
+    cout << it->first << " - " << it->second << endl;
   }
+
+  cout << kevs[20] << endl;
+
+  cout << kevs[2] << endl;
 }
---
+++ kevs4.cpp
@@ -0,0 +1,61 @@
+// atoi(const char*) - base 10 return int (32-bit)
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "assert.h"
+#include "string.h"
+
+int atoi_s(const uint8_t* in, uint8_t max_buf) {
+  assert(in);
+  assert(max_buf > 0);
+
+  // other asserts might be max_buf would hold +/- max_int
+
+  uint8_t* str_ptr = (uint8_t*)in;
+  uint8_t* str_ptr_sentinel = (uint8_t*)in + max_buf;
+  int is_negative = false;
+
+  // Ah also need to check for negative
+  if (*str_ptr == '-') {
+    is_negative = true;
+    str_ptr++; // parse the sign bit so move to number
+  }
+
+  // Going to work backwards - find last number character
+  while (str_ptr < str_ptr_sentinel) {
+    if ((*str_ptr < '0') || (*str_ptr > '9'))
+      break;
+    str_ptr++;
+  }
+
+
+  int retval = 0;
+  int base10_multiply = 1;
+
+  while (str_ptr >= in) {
+    if ((*str_ptr >= '0') && (*str_ptr <= '9')) {
+      retval += ((*str_ptr) - '0') * base10_multiply;
+      base10_multiply *= 10;
+    }
+
+    str_ptr--;
+  }
+
+  if (is_negative)
+    retval = -retval;
+
+  return retval;
+}
+
+int atoi(const char* in)
+{
+  return atoi_s((uint8_t*)in, strlen(in));
+}
+
+int main(int argc, char *argv[]) {
+  // Test
+  int out = atoi("-42");
+
+  printf("%d\n", out);
+}
+
